b3660b0008a20f4ba032bba840f8dc4fdc8099b0
Sreevijeesh <sreevijeesh2004@gmail.com>
2025-07-29 10:38:46 +0530

    Add initial files

---
+++ day1.js
@@ -0,0 +1,12 @@
+let array=[10,20,30,40,50];
+console.log(array[2]);
+array[15]=56;
+console.log(array);
+console.log(array.length);
+let foods = {tiffin:"idly, poori ,pongal", lunch:"Cbriyani ,Mbriyani", dinner:"chappatti , dosai , kottuparotta"};
+console.log(food(tiffin));
+
+    // ,"idly", "poori","pongal"
+    // "Cbriyani","Mbriyani",
+    // ,"chappatti","dosai","kottuparotta"
+    
