22afb99c2db2417e9d86967b1c50e41dddc43ed2
Sreevijeesh <sreevijeesh2004@gmail.com>
2025-08-04 23:48:18 +0530

    day4.js

---
+++ day4.js
@@ -0,0 +1,59 @@
+//statements
+//conditional,iterative, switch, jump
+//conditional statements
+let age=20;
+if (age >= 18){
+    console.log("you are eligible to Bar");
+
+}
+else{
+    console.log("you are not eligible to Bar")
+}
+
+// let marks=80;
+// if (marks >=90){
+//     console.log("grade A")}
+// else if (marks>=70);{
+//          console.log("grade B");
+// }
+// else {
+//     console.log("grade C");}
+
+
+//true connditions
+if(" "){
+    console.log("true")
+}
+else{
+console.log("false")
+}
+//nested if statement
+let num = 50;
+
+if(num > 0){
+if(num % 2 ==0){
+    console.log("number is even");
+}
+else{
+    console.log("number is odd");
+}
+}else{
+    console.log("num is - or 0");
+}
+// iterative statement
+let i = 50;
+while(i <= 60 );{
+    console.log("num:"+i);
+    i++;
+
+}
+let j=40;
+do{
+    console.log("num:",+ j);
+    j++;
+}
+while(j<=35);
+for(let k=25; k<=14; k++)
+{
+    console.log("num:",+k)
+}
