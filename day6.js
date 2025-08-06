20fc1f398eae1f46f25aa4a3afa3ec2ae5f07c11
Sreevijeesh <sreevijeesh2004@gmail.com>
2025-08-06 22:27:38 +0530

    day6.js

---
+++ day6.js
@@ -0,0 +1,37 @@
+  if (" "){
+        console.log("condition is true");
+
+    }
+    else{
+        console.log("condition is false");
+
+    }
+    //types of if //ladder, nested      ==else if  ,    
+    //ladder 
+    let mark=70;
+      if (mark>=80){
+         console.log("grade1");
+      }
+      else if( mark >= 70){
+         console.log("grade2")
+      }
+    else{console.log("grade3")};
+// //nested if
+if(mark >= 70) {
+    if (mark >= 80) {
+        console.log("A");
+    } else {
+        console.log("B");
+    }
+}
+//iterative
+//for while do-while
+for (let i = 0; i<5; i++){
+console.log("iteration:"+i);
+}
+
+let arr=[3,4,5,6,7];
+for (let i=0; i<5; i++);
+console.log("prt:"+arr[i]);
+
+while (true);
