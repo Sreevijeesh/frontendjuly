658a5278d96ad2e5b74ba7a2e4d8024da0be2886
Sreevijeesh <sreevijeesh2004@gmail.com>
2025-08-20 23:41:43 +0530

    day7.js

--- day7.js
+++ day7.js
@@ -1,11 +1,16 @@
-let word="2a3b4c";
-let result="";
-let digit=0;
-for (let i=0; i<word.length; i++){
-    if(!isNaN(word[i])){
-        digit=parseInt(word[i]);
-    }else{
-        result+=word[i].repeat(digit);
-        digit=0;
+//patterns,sorting
+let rows=7;
+let cols=1;
+let result='';
+for (let i=1; i<=rows; i++){
+    for (let j=1; j<=cols; j++){
+        result +=j%2!=0 ? '*':' ';  //result += '*';
+
     }
+    result +='\n';
+    cols +=2;
 }
+console.log(result);
+
+
+
